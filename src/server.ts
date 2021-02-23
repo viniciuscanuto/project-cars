e43d04dd89a3eefeda4c91b6a48e356914e9eed4
viniciusCanuto <vncscanuto@gmail.com>
2021-02-23 16:07:48 -0300

    chore: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors  from  'cors'
 import routes from './routes'
 
 import { MongoConnect } from './db/MongoConnect'
@@ -9,8 +10,9 @@ const mongoUrl = 'mongodb://localhost:27017/project-cars'
 
 MongoConnect.connect(mongoUrl)
   .then(async () => {
+    //app.use(cors())
+    app.use(express.json())
     app.use(routes)
-    app.use(express.json)
     app.listen(3333, () => console.log(`Server running at http://localhost:3333`))
   })
   .catch(console.error)
